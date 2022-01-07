5cba7659577d682c720dd329f3b33333415aaf35
ThomasNeyman <neymanthomas45@gmail.com>
2022-01-06 22:43:32 -0700

    Base BattleSystem Added

--- Assets/Code/Character.cs
+++ Assets/Code/Character.cs
@@ -15,51 +15,68 @@ public class Character
     private int _skill;
     private int _luck;
     private object _weapon;
-    #endregion
+    private bool _isAlive;
 
-    public string Name 
+    public string Name { get => _name; set => _name = value; }
+    public int MaxHealth { get => _maxHealth; set => _maxHealth = value; }
+    public int CurrentHealth { get => _currentHealth; set => _currentHealth = value; }
+    public int Power { get => _power; set => _power = value; }
+    public int Defense { get => _defense; set => _defense = value; }
+    public int Stamina { get => _stamina; set => _stamina = value; }
+    public int MagicPower { get => _magicPower; set => _magicPower = value; }
+    public int MagicResistance { get => _magicResistance; set => _magicResistance = value; }
+    public int Mana { get => _mana; set => _mana = value; }
+    public int Speed { get => _speed; set => _speed = value; }
+    public int Skill { get => _skill; set => _skill = value; }
+    public int Luck { get => _luck; set => _luck = value; }
+    public object Weapon { get => _weapon; set => _weapon = value; }
+    public bool IsAlive 
     {
         get 
         {
-            return _name;
-        }
-        set 
-        {
-            _name = value;
+            if (CurrentHealth <= 0) 
+            {
+                return false;
+            } 
+            else 
+            {
+                return true;
+            }
         }
     }
+    #endregion
 
     public Character() 
     {
         _name = "default";
-        _maxHealth = 10;
-        _currentHealth = 10;
-        _power = 10;
-        _defense = 10;
-        _stamina = 10;
-        _magicPower = 10;
-        _magicResistance = 10;
-        _mana = 10;
-        _speed = 10;
-        _skill = 10;
-        _luck = 10;
-        _weapon = null;
+        MaxHealth = 10;
+        CurrentHealth = 10;
+        Power = 10;
+        Defense = 10;
+        Stamina = 10;
+        MagicPower = 10;
+        MagicResistance = 10;
+        Mana = 10;
+        Speed = 10;
+        Skill = 10;
+        Luck = 10;
+        Weapon = null;
     }
 
     public Character(string name, int maxHealth, int currHealth, int power, int defense, int stamina, int magicPWR, int magicRES, int mana, int speed, int skill, int luck) 
     {
         _name = name;
-        _maxHealth = maxHealth;
-        _currentHealth = currHealth;
-        _power = power;
-        _defense = defense;
-        _stamina = stamina;
-        _magicPower = magicPWR;
-        _magicResistance = magicRES;
-        _mana = mana;
-        _speed = speed;
-        _skill = skill;
-        _luck = luck;
-        _weapon = null;
+        MaxHealth = maxHealth;
+        CurrentHealth = currHealth;
+        Power = power;
+        Defense = defense;
+        Stamina = stamina;
+        MagicPower = magicPWR;
+        MagicResistance = magicRES;
+        Mana = mana;
+        Speed = speed;
+        Skill = skill;
+        Luck = luck;
+        Weapon = null;
     }
 }
